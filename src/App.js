d3bc129d293e5ad57d680672a06deb17bd67c1dc
rylCodes <andresdaryl.94@gmail.com>
2024-11-30 00:06:00 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Login from "./components/Login";
+import Dashboard from "./scenes/dashboard";
+import Customerlist from "./scenes/customerlist";
+import Settings from "./scenes/settings";
+import Monthlylist from "./scenes/monthlylist";
+import Payment from "./components/payment";
+import PaymentForm from "./components/paymentForm";
+import PaymentReceipt from "./components/paymentReceipt";
+import CustomerArchive from "./scenes/customerlist/customerArchive";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Update from "./components/update";
+import AccountSettings from "./scenes/settings/accountSettings";
+import Products from "./scenes/products";
+import MonthPaymentForm from "./scenes/products/monthPaymentForm";
+import ChangePassword from "./components/changePassword";
+import ForgotPassword from "./components/forgotPassword";
+import EmployeeAttendance from "./scenes/employeeAttendance";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  useEffect(() => {
+    document.title = "The Gym Republic | Staff"; // Set the title here
+  }, []);
+
+  return (
+    <Router>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <ContentLayout isSidebar={isSidebar} setIsSidebar={setIsSidebar} />
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </Router>
+  );
+}
+
+function ContentLayout({ isSidebar, setIsSidebar }) {
+  const location = useLocation();
+
+  // Define routes where Sidebar and Topbar should be hidden
+  const hideSidebarAndTopbar = ["/", "/change-password", "/forgot-password", "/payment-form", "/payment-receipt"].includes(location.pathname);
+
+  return (
+    <>
+      {!hideSidebarAndTopbar && <Sidebar isSidebar={isSidebar} />}
+      <main className="content">
+        {!hideSidebarAndTopbar && <Topbar setIsSidebar={setIsSidebar} />}
+        
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/change-password" element={<ChangePassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/customer-list" element={<Customerlist />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/payment-form" element={<PaymentForm />} />
+          <Route path="/payment-receipt" element={<PaymentReceipt />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/employee-attendance" element={<EmployeeAttendance />} />
+
+          <Route path="/customer-archive" element={<CustomerArchive />} />
+          <Route path="/account-settings" element={<AccountSettings />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/month-payment-form" element={<MonthPaymentForm />} />
+          <Route path="/monthly-list" element={<Monthlylist />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
